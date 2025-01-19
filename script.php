57443c1f58b6a81c3b56addc46b321ac7b3b3c84
Pascal Leconte <pascal.leconte@conseilgouz.com>
2025-01-19 17:41:54 +0100

    Update script.php

--- script.php
+++ script.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* CG Clean Module  - Joomla 4.x/5.x Module 
+* CG Clean Module  - Joomla 4.x/5.x Module
 * Package			: CG Clean
 * copyright 		: Copyright (C) 2025 ConseilGouz. All rights reserved.
 * license    		: http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
@@ -16,150 +16,147 @@ use Joomla\Database\DatabaseInterface;
 
 class mod_cg_cleanInstallerScript
 {
-	private $min_joomla_version      = '4.0.0';
-	private $min_php_version         = '8.0';
-	private $name                    = 'CG Clean';
-	private $exttype                 = 'module';
-	private $extname                 = 'cg_clean';
-	private $previous_version        = '';
-	private $dir           = null;
-	private $installerName = 'cg_cleaninstaller';
-	public function __construct()
-	{
-		$this->dir = __DIR__;
-		$this->lang = Factory::getLanguage();
-		$this->lang->load($this->extname);
-	}
-
-    function preflight($type, $parent)
+    private $min_joomla_version      = '4.0.0';
+    private $min_php_version         = '8.0';
+    private $name                    = 'CG Clean';
+    private $exttype                 = 'module';
+    private $extname                 = 'cg_clean';
+    private $previous_version        = '';
+    private $dir           = null;
+    private $lang           = null;
+    private $installerName = 'cg_cleaninstaller';
+    public function __construct()
     {
-		if ( ! $this->passMinimumJoomlaVersion())
-		{
-			$this->uninstallInstaller();
-			return false;
-		}
-
-		if ( ! $this->passMinimumPHPVersion())
-		{
-			$this->uninstallInstaller();
-			return false;
-		}
-		// To prevent installer from running twice if installing multiple extensions
-		if ( ! file_exists($this->dir . '/' . $this->installerName . '.xml'))
-		{
-			return true;
-		}
-		$xml = simplexml_load_file(JPATH_BASE . '/modules/mod_'.$this->extname.'/mod_'.$this->extname.'.xml');
-		$this->previous_version = $xml->version;
+        $this->dir = __DIR__;
+        $this->lang = Factory::getApplication()->getLanguage();
+        $this->lang->load($this->extname);
     }
-    
-    function postflight($type, $parent)
-    {
-		if (($type=='install') || ($type == 'update')) { // remove obsolete dir/files
-			$this->postinstall_cleanup();
-		}
 
-		switch ($type) {
-            case 'install': $message = Text::_('ISO_POSTFLIGHT_INSTALLED'); break;
-            case 'uninstall': $message = Text::_('ISO_POSTFLIGHT_UNINSTALLED'); break;
-            case 'update': $message = Text::_('ISO_POSTFLIGHT_UPDATED'); break;
-            case 'discover_install': $message = Text::_('ISO_POSTFLIGHT_DISC_INSTALLED'); break;
+    public function preflight($type, $parent)
+    {
+        if (! $this->passMinimumJoomlaVersion()) {
+            $this->uninstallInstaller();
+            return false;
         }
-		return true;
+
+        if (! $this->passMinimumPHPVersion()) {
+            $this->uninstallInstaller();
+            return false;
+        }
+        // To prevent installer from running twice if installing multiple extensions
+        if (! file_exists($this->dir . '/' . $this->installerName . '.xml')) {
+            return true;
+        }
+        $xml = simplexml_load_file(JPATH_BASE . '/modules/mod_'.$this->extname.'/mod_'.$this->extname.'.xml');
+        $this->previous_version = $xml->version;
     }
-	private function postinstall_cleanup() {
-		$obsloteFolders = ['asset','language'];
-		// Remove plugins' files which load outside of the component. If any is not fully updated your site won't crash.
-		foreach ($obsloteFolders as $folder)
-		{
-			$f = JPATH_ADMINISTRATOR . '/modules/mod_'.$this->extname.'/' . $folder;
 
-			if (!@file_exists($f) || !is_dir($f) || is_link($f))
-			{
-				continue;
-			}
+    public function postflight($type, $parent)
+    {
+        if (($type == 'install') || ($type == 'update')) { // remove obsolete dir/files
+            $this->postinstall_cleanup();
+        }
 
-			Folder::delete($f);
-		}
-		$obsloteFiles = [sprintf("%s/modules/mod_%s/helper.php", JPATH_ADMINISTRATOR, $this->extname)];
-		foreach ($obsloteFiles as $file)
-		{
-			if (@is_file($file))
-			{
-				File::delete($file);
-			}
-		}
-		$j = new Version();
-		$version=$j->getShortVersion(); 
-		$version_arr = explode('.',$version);
-		if (($version_arr[0] == "4") || (($version_arr[0] == "3") && ($version_arr[1] == "10"))) {
-			// Delete 3.9 and older language files
-			$langFiles = [
-				sprintf("%s/language/en-GB/en-GB.mod_%s.ini", JPATH_ADMINISTRATOR, $this->extname),
-				sprintf("%s/language/en-GB/en-GB.mod_%s.sys.ini", JPATH_ADMINISTRATOR, $this->extname),
-				sprintf("%s/language/fr-FR/fr-FR.mod_%s.ini", JPATH_ADMINISTRATOR, $this->extname),
-				sprintf("%s/language/fr-FR/fr-FR.mod_%s.sys.ini", JPATH_ADMINISTRATOR, $this->extname),
-			];
-			foreach ($langFiles as $file) {
-				if (@is_file($file)) {
-					File::delete($file);
-				}
-			}
-		}
-	}
+        switch ($type) {
+            case 'install': $message = Text::_('ISO_POSTFLIGHT_INSTALLED');
+                break;
+            case 'uninstall': $message = Text::_('ISO_POSTFLIGHT_UNINSTALLED');
+                break;
+            case 'update': $message = Text::_('ISO_POSTFLIGHT_UPDATED');
+                break;
+            case 'discover_install': $message = Text::_('ISO_POSTFLIGHT_DISC_INSTALLED');
+                break;
+        }
+        return true;
+    }
+    private function postinstall_cleanup()
+    {
+        $obsloteFolders = ['asset','language'];
+        // Remove plugins' files which load outside of the component. If any is not fully updated your site won't crash.
+        foreach ($obsloteFolders as $folder) {
+            $f = JPATH_ADMINISTRATOR . '/modules/mod_'.$this->extname.'/' . $folder;
 
-	// Check if Joomla version passes minimum requirement
-	private function passMinimumJoomlaVersion()
-	{
-		$j = new Version();
-		$version=$j->getShortVersion(); 
-		if (version_compare($version, $this->min_joomla_version, '<'))
-		{
-			Factory::getApplication()->enqueueMessage(
-				'Incompatible Joomla version : found <strong>' . $version . '</strong>, Minimum : <strong>' . $this->min_joomla_version . '</strong>',
-				'error'
-			);
+            if (!@file_exists($f) || !is_dir($f) || is_link($f)) {
+                continue;
+            }
 
-			return false;
-		}
+            Folder::delete($f);
+        }
+        $obsloteFiles = [sprintf("%s/modules/mod_%s/helper.php", JPATH_ADMINISTRATOR, $this->extname)];
+        foreach ($obsloteFiles as $file) {
+            if (@is_file($file)) {
+                File::delete($file);
+            }
+        }
+        $j = new Version();
+        $version = $j->getShortVersion();
+        $version_arr = explode('.', $version);
+        if (($version_arr[0] == "4") || (($version_arr[0] == "3") && ($version_arr[1] == "10"))) {
+            // Delete 3.9 and older language files
+            $langFiles = [
+                sprintf("%s/language/en-GB/en-GB.mod_%s.ini", JPATH_ADMINISTRATOR, $this->extname),
+                sprintf("%s/language/en-GB/en-GB.mod_%s.sys.ini", JPATH_ADMINISTRATOR, $this->extname),
+                sprintf("%s/language/fr-FR/fr-FR.mod_%s.ini", JPATH_ADMINISTRATOR, $this->extname),
+                sprintf("%s/language/fr-FR/fr-FR.mod_%s.sys.ini", JPATH_ADMINISTRATOR, $this->extname),
+            ];
+            foreach ($langFiles as $file) {
+                if (@is_file($file)) {
+                    File::delete($file);
+                }
+            }
+        }
+    }
 
-		return true;
-	}
+    // Check if Joomla version passes minimum requirement
+    private function passMinimumJoomlaVersion()
+    {
+        $j = new Version();
+        $version = $j->getShortVersion();
+        if (version_compare($version, $this->min_joomla_version, '<')) {
+            Factory::getApplication()->enqueueMessage(
+                'Incompatible Joomla version : found <strong>' . $version . '</strong>, Minimum : <strong>' . $this->min_joomla_version . '</strong>',
+                'error'
+            );
 
-	// Check if PHP version passes minimum requirement
-	private function passMinimumPHPVersion()
-	{
+            return false;
+        }
 
-		if (version_compare(PHP_VERSION, $this->min_php_version, '<'))
-		{
-			Factory::getApplication()->enqueueMessage(
-					'Incompatible PHP version : found  <strong>' . PHP_VERSION . '</strong>, Minimum <strong>' . $this->min_php_version . '</strong>',
-				'error'
-			);
-			return false;
-		}
+        return true;
+    }
 
-		return true;
-	}
-	private function uninstallInstaller()
-	{
-		if ( ! is_dir(JPATH_PLUGINS . '/system/' . $this->installerName)) {
-			return;
-		}
-		$this->delete([
-			JPATH_PLUGINS . '/system/' . $this->installerName . '/language',
-			JPATH_PLUGINS . '/system/' . $this->installerName,
-		]);
-		$db = Factory::getContainer()->get(DatabaseInterface::class);
-		$query = $db->getQuery(true)
-			->delete('#__extensions')
-			->where($db->quoteName('element') . ' = ' . $db->quote($this->installerName))
-			->where($db->quoteName('folder') . ' = ' . $db->quote('system'))
-			->where($db->quoteName('type') . ' = ' . $db->quote('plugin'));
-		$db->setQuery($query);
-		$db->execute();
-		Factory::getCache()->clean('_system');
-	}
+    // Check if PHP version passes minimum requirement
+    private function passMinimumPHPVersion()
+    {
+
+        if (version_compare(PHP_VERSION, $this->min_php_version, '<')) {
+            Factory::getApplication()->enqueueMessage(
+                'Incompatible PHP version : found  <strong>' . PHP_VERSION . '</strong>, Minimum <strong>' . $this->min_php_version . '</strong>',
+                'error'
+            );
+            return false;
+        }
+
+        return true;
+    }
+    private function uninstallInstaller()
+    {
+        if (! is_dir(JPATH_PLUGINS . '/system/' . $this->installerName)) {
+            return;
+        }
+        $this->delete([
+            JPATH_PLUGINS . '/system/' . $this->installerName . '/language',
+            JPATH_PLUGINS . '/system/' . $this->installerName,
+        ]);
+        $db = Factory::getContainer()->get(DatabaseInterface::class);
+        $query = $db->getQuery(true)
+            ->delete('#__extensions')
+            ->where($db->quoteName('element') . ' = ' . $db->quote($this->installerName))
+            ->where($db->quoteName('folder') . ' = ' . $db->quote('system'))
+            ->where($db->quoteName('type') . ' = ' . $db->quote('plugin'));
+        $db->setQuery($query);
+        $db->execute();
+        Factory::getCache()->clean('_system');
+    }
     public function delete($files = [])
     {
         foreach ($files as $file) {
@@ -172,5 +169,5 @@ class mod_cg_cleanInstallerScript
             }
         }
     }
-	
+
 }
